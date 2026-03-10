ba63c1b75dfb499b638fa35a84fe097b962e6c85
Sainath Kamble <sainathio@Sainaths-MacBook-Air.local>
2026-03-10 15:50:19 +0530

    feat(env): added env var handeling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
   credentials: true, // allow cookies cross-origin
 }));
 app.use(express.json());
